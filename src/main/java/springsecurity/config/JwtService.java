da8dd266b3a801420fa9ad55f5f1d48daf1f8703
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-10 09:40:15 -0300

    JwtService on process

---
+++ src/main/java/springsecurity/config/JwtService.java
@@ -0,0 +1,8 @@
+package springsecurity.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public static final String SECRET_KEY = "646d279acbaf33b4149b8487c98a4f4e6f208817f8da86ed81057c5060c2412f";
+}
